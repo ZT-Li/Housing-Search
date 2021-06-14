269769ddef693171263e4cfe043f5f0151381e17
zt.li.fisher@gmail.com <ZT-Li@users.noreply.github.com>
2021-06-14 12:27:05 -0700

    updates frontend

--- hs/housing-search/src/Components/SearchBar/SearchBar.js
+++ hs/housing-search/src/Components/SearchBar/SearchBar.js
@@ -1,11 +1,16 @@
 import styles from './SearchBar.css'
+import logo from './icons/logo.png'
 
 export function SearchBar() {
     return (
         <div className='search-block'>
-            <form action='/' method='get'>
+            <div className='header'>
+                <img className='logo' src={logo}></img>
+                <h1 className='header-name'>HOME RADAR</h1>
+            </div>
+            <form action='/' method='get' className='search-form'>
                 <input type='text' className='search-bar' placeholder='search a house' />
-                <button className='search-btn' type='submit'>search</button>
+                <button className='search-btn' type='submit'></button>
             </form>
         </div>
     )
